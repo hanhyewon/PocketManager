95a14df23234da051448fc95c0a530c47f254fa8
hanhyewon <gpdnjs678@naver.com>
2019-09-18 16:34:53 +0900

    0918_혜원_테스트

---
+++ app/src/main/java/com/example/gpdnj/pocketmanager/ManagerEventSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.gpdnj.pocketmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerEventSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_event_setting);
+    }
+}
